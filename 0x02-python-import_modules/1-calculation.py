07ef750f6cc33e60e62c0495610d6172c21c3872
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-08 13:09:45 -0700

    importing multiple functions without * or __import__

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     b = 5
 
     print("{} + {} = {}".format(a, b, c.add(a, b)))
-    print("{} + {} = {}".format(a, b, c.sub(a, b)))
-    print("{} + {} = {}".format(a, b, c.mul(a, b)))
-    print("{} + {} = {}".format(a, b, c.div(a, b)))
+    print("{} - {} = {}".format(a, b, c.sub(a, b)))
+    print("{} * {} = {}".format(a, b, c.mul(a, b)))
+    print("{} / {} = {}".format(a, b, c.div(a, b)))
