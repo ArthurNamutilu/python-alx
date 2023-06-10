e192e075a4e03b5b7123938a2ac331ba0729f649
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-10 13:46:59 -0700

    program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     plural = "s" if m == 1 or m > 2 else ""
     q = ":" if m > 1 else "."
 
-    print("{:d} argument{}{}".format(m -1, plural, q))
+    print("{:d} argument{}{}".format(m - 1, plural, q))
 
     for i in range(1, m):
         print("{:d}: {}".format(i, sys.argv[i]))
